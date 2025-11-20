fe29ce3d9a5d4500f1a330bade641fa3ad10bdde
mishalabdullah <github.ijvob@simplelogin.co>
2025-11-20 14:42:15 +0530

    rm old confs

--- battery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-percent=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep "percentage:" | awk '{ print $2 }')
-Volume=$(amixer sget Master | grep Front  | awk '{print $5 }' | sed -n 2p)
-date=$(date  +"%A,%B %d,%H:%M:%S" )
-bright=$(light -G)
-echo " 🗓️:$date 🔋: $percent:🔊$Volume:☀️ $bright"
